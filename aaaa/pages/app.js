9b92a3d7861cfdbd0cba357c2bf5b4a8742ed062
喻博文 <yubowen@yubowen2003.com>
2023-09-07 15:04:26 +0800

    删掉不要的东西

--- aaaa/pages/app.js
+++
@@ -1,9 +0,0 @@
-import { matchRoutes } from 'umi';
-
-export function onRouteChange({ clientRoutes, location }) {
-    const route = matchRoutes(clientRoutes, location.pathname)?.pop()?.route;
-    console.log("route:", route)
-    if (route) {
-        document.title = route.title || '';
-    }
-}
--- aaaa/utils/auth/index.js
+++
@@ -1,3 +0,0 @@
-const TOKEN = 'access_token';
-export const getToken = () => localStorage.getItem(TOKEN);
-export const setToken = (token) => localStorage.setItem(TOKEN, token);
